af7278da7af0d77013c7689692a536d7e1f24049
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-07-31 15:57:41 -0500

    new modification

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,14 +15,15 @@ exports.newProduct = async(req,res, next) =>{
 }
 
 //Get all products => /api/v1/products
-exports.getProducts= (req,res,next) => {
+exports.getProducts= async (req,res,next) => {
 
-    //const products = Product.find();
+    const products = await Product.find();
 
 
     res.status(200).json({
         success: true,
         // count: products.length,
-        message: "Here goes the products"
+        // message: "Here goes the products"
+        products
     })
 }
