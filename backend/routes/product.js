878bad56c2cbbb558c33b5c520c49f6cef1b1708
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-01 08:44:25 -0500

    delete product by ide route and controller

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController');
+const { getProducts,
+        newProduct,
+        getSingleProduct,
+        updateProduct,
+        deleteProduct} = require('../controllers/productController');
 
 
 router.route('/products').get(getProducts);
@@ -12,5 +16,7 @@ router.route('/admin/product/new').post(newProduct);
 router.route('/product/:id').get(getSingleProduct);
 
 router.route('/admin/product/:id').put(updateProduct);
+
+router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
