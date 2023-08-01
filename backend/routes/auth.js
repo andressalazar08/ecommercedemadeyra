bc11821b5bdbc97ad66cd73fc16f32e9c1d2de33
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-01 16:43:07 -0500

    user profile route and request

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,13 +4,20 @@ const router = express.Router();
 
 
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require('../controllers/authController');
+const { registerUser,
+    loginUser,
+    logout,
+    forgotPassword,
+    resetPassword, getUserProfile } = require('../controllers/authController');
 
+
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logout);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
+router.route('/me').get(isAuthenticatedUser,getUserProfile);
 
 module.exports = router;
