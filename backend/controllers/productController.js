4acc0060a43d3c1ca11bba3db18308bb68f2dd35
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-01 15:06:58 -0500

    roles and adding user in product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,7 @@ const APIFeatures = require('../utils/apiFeatures');
 
 exports.newProduct = catchAsyncErrors(async(req,res, next) =>{
 
+    req.body.user = req.user.id;
 
     const product = await Product.create(req.body)
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -8,15 +8,15 @@ const { getProducts,
         updateProduct,
         deleteProduct} = require('../controllers/productController');
 
-const { isAuthenticatedUser } = require('../middlewares/auth')
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 
 router.route('/products').get( getProducts);//proteger las rutas con isAuthenticatedUser
 
-router.route('/admin/product/new').post(isAuthenticatedUser,newProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'),newProduct);
 
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/admin/product/:id').put(isAuthenticatedUser,updateProduct).delete(isAuthenticatedUser,deleteProduct); //en el caso de que coincidan as rutas puedo aplicar los dos métodos
+router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'),updateProduct).delete(isAuthenticatedUser, authorizeRoles('admin'),deleteProduct); //en el caso de que coincidan as rutas puedo aplicar los dos métodos
 
 module.exports = router;
