0da149c799478ee187671ca1f1df1217c51141bb
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-02 11:23:40 -0500

    update profile logged user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,7 +10,8 @@ const { registerUser,
     forgotPassword,
     resetPassword,
     getUserProfile,
-    updatePassword } = require('../controllers/authController');
+    updatePassword,
+    updateProfile} = require('../controllers/authController');
 
 
 const { isAuthenticatedUser } = require('../middlewares/auth');
@@ -22,5 +23,6 @@ router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 router.route('/me').get(isAuthenticatedUser,getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser,updatePassword);
+router.route('/me/update').put(isAuthenticatedUser,updateProfile);
 
 module.exports = router;
