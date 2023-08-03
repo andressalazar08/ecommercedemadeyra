626dda97aae2c6c823df3effa96fb452caf71c61
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-03 16:11:04 -0500

    Firs route implementation on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Footer />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+            <div className="container container-fluid">
+              <Route exact path="/" component={Home} />
+            </div>
+          <Footer />
+      </div>
+    </Router>
   );
 }
 
