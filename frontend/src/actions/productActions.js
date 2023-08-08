3532e2774696a060c520a3e43baeb5bf5aa49d6c
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-08-08 17:29:45 -0500

    productDetails state

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,7 +2,11 @@ import {
     ALL_PRODUCTS_REQUEST,
     ALL_PRODUCTS_SUCCESS,
     ALL_PRODUCTS_FAIL,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_SUCCESS,
+
 } from '../constants/productConstants';
 
 import axios from 'axios';
@@ -22,6 +26,27 @@ export const getProducts = () => async (dispatch)=>{
     }catch(error){
         dispatch({
             type:ALL_PRODUCTS_FAIL,
+            payload:error.response.data.message
+        })
+    }
+}
+
+
+
+export const getProductDetails = (id) => async (dispatch)=>{
+    try{
+        dispatch({type: PRODUCT_DETAILS_REQUEST})
+
+        const { data } = await axios.get(`/api/v1/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload:data
+        })
+
+    }catch(error){
+        dispatch({
+            type:PRODUCT_DETAILS_FAIL,
             payload:error.response.data.message
         })
     }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -3,7 +3,10 @@ import {
     ALL_PRODUCTS_REQUEST,
     ALL_PRODUCTS_SUCCESS,
     ALL_PRODUCTS_FAIL,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants';
 
 export const productsReducer = (state= { products:[] }, action)=>{
@@ -39,4 +42,39 @@ export const productsReducer = (state= { products:[] }, action)=>{
             return state;
 
     }
+}
+
+//(state= { products:[] }, action)
+export const productDetailsReducer = (state={product:{}}, action)=>{
+    switch(action.type){
+
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading: true,
+                product: action.payload
+            }
+
+        case PRODUCT_DETAILS_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error:null
+            }
+
+        default:
+            return state
+    }
+
+
 }
